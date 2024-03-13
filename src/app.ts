e195045ab81efa28d2bb9c3320b834e79e6f7af4
reza swary <126505674+reza16t@users.noreply.github.com>
2024-03-13 14:44:34 +0330

    Add files via upload

---
+++ src/app.ts
@@ -0,0 +1,27 @@
+const express = require('express');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+
+const app = express();
+
+// 1) MIDDLEWARES
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
+
+// 3) ROUTES
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+dotenv.config({ path: './config.env' });
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
---
+++ src/controllers/tourController.ts
@@ -0,0 +1,93 @@
+const fs = require('fs');
+import { Response, Request, NextFunction } from 'express';
+import { ToursType } from '../types';
+
+const tours: ToursType[] = JSON.parse(
+  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+);
+
+export const checkID = (req: Request, res: Response, next: NextFunction) => {
+  // console.log(`Tour id is: ${val}`);
+  const id: number = +req.params.id;
+  if (id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+  next();
+};
+
+export const checkBody = (req: Request, res: Response, next: NextFunction) => {
+  if (!req.body.name || !req.body.price) {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'Missing name or price',
+    });
+  }
+  next();
+};
+
+export const getAllTours = (req: Request, res: Response) => {
+  console.log();
+
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+};
+
+export const getTour = (req: Request, res: Response) => {
+  console.log(req.params);
+  const id: Number = +req.params.id * 1;
+
+  const tour = tours.find((el) => el.id === id);
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+};
+
+export const createTour = (req: Request, res: Response) => {
+  // console.log(req.body);
+
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err: String) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+};
+
+export const updateTour = (req: Request, res: Response) => {
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour here...>',
+    },
+  });
+};
+
+export const deleteTour = (req: Request, res: Response) => {
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+};
